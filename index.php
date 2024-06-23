186ce2dbe269e5c6913d2bfac27780d75d45700d
damirusso95 <damirusso95@gmail.com>
2024-06-23 11:26:29 +0200

    definisco una classe 'movie' con all'interno due variabili d'istanza

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    // classe
+class movie {
+    // proprietà o variabili d'istanza
+    public $titolo
+    public $genere
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<h1>hello oop</h1>
+<?php
+
+?> 
+</body>
+</html>
